d20acc82ed5baba1044dc7e6e2d5d02bf39d0944
Kabir Dhillon <kabirdhillon99@gmail.com>
2021-10-21 13:51:20 -0700

    Updates

--- src/main/java/edu/csueastbay/cs401/share/MainMenuController.java
+++ src/main/java/edu/csueastbay/cs401/share/MainMenuController.java
@@ -28,6 +28,9 @@ public class MainMenuController {
             case "Bob Smith":
                 loadStudentPane("/edu/csueastbay/cs401/bsmith/reveal.fxml");
                 break;
+            case "Kabir Dhillon":
+                loadStudentPane("/edu/csueastbay/cs401/kdhillon/reveal.fxml");
+                break;
             default:
                 System.out.println("huh?");
 
