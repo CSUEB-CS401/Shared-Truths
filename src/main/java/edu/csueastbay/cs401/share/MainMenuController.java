2d671adaf2dd648b256096c9b27e99cceb7f3b48
Matthew Jelonek <matthewjelonek@gmail.com>
2021-10-21 13:53:40 -0700

    matts button added

--- src/main/java/edu/csueastbay/cs401/share/MainMenuController.java
+++ src/main/java/edu/csueastbay/cs401/share/MainMenuController.java
@@ -28,6 +28,9 @@ public class MainMenuController {
             case "Bob Smith":
                 loadStudentPane("/edu/csueastbay/cs401/bsmith/reveal.fxml");
                 break;
+            case "Matthew Jelonek":
+                loadStudentPane("/edu/csueastbay/cs401/mjelonek/reveal.fxml");
+                break;
             default:
                 System.out.println("huh?");
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,5 +6,7 @@ module edu.csueastbay.cs401.shareprep {
     opens edu.csueastbay.cs401.share to javafx.fxml;
     opens edu.csueastbay.cs401.praupach to javafx.fxml;
     opens edu.csueastbay.cs401.bsmith to javafx.fxml;
+    // Added to give permission to open up file
+    opens edu.csueastbay.cs401.mjelonek to javafx.fxml;
     exports edu.csueastbay.cs401.share;
 }
