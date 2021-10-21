f8d6b525580dc95f94ba96ff60511d3c5b4e8c60
EJamdar05 <jamdareshaq@gmail.com>
2021-10-21 13:42:37 -0700

    added my truth

--- src/main/java/edu/csueastbay/cs401/share/MainMenuController.java
+++ src/main/java/edu/csueastbay/cs401/share/MainMenuController.java
@@ -28,6 +28,9 @@ public class MainMenuController {
             case "Bob Smith":
                 loadStudentPane("/edu/csueastbay/cs401/bsmith/reveal.fxml");
                 break;
+            case "Eshaq Jamdar":
+                loadStudentPane("/edu/csueastbay/cs401/ejamdar/reveal.fxml");
+                break;
             default:
                 System.out.println("huh?");
 
