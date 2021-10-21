4f61075bef3a1aaafb4036cd3d52588f9ed4ae43
Jonathan <jzepeda19@horizon.csueastbay.edu>
2021-10-21 13:43:10 -0700

    Completed added my personal truth.

---
+++ src/main/java/edu/csueastbay/cs401/jzepeda/RevealController.java
@@ -0,0 +1,33 @@
+package edu.csueastbay.cs401.jzepeda;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RevealController implements Initializable {
+
+    private Truth jonathansTruth;
+
+    @FXML
+    private Label displayLabel;
+
+    @FXML
+    private Button revealButton;
+
+    @FXML
+    void reveal(ActionEvent event) {
+        displayLabel.setText(jonathansTruth.getAnswer());
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        jonathansTruth = new Truth("Home Town", "Martinez, Ca");
+        displayLabel.setText(jonathansTruth.getQuestion());
+    }
+}
+
