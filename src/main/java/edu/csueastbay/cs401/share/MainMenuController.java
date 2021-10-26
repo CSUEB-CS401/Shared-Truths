81112eb4503b0be29993f379c67a5b72fbc0b378
keeviant <keeviant@gmail.com>
2021-10-26 13:01:23 -0700

    Added my question and answer (Katherine Hettick-Leir)

--- src/main/java/edu/csueastbay/cs401/share/MainMenuController.java
+++ src/main/java/edu/csueastbay/cs401/share/MainMenuController.java
@@ -28,6 +28,9 @@ public class MainMenuController {
             case "Bob Smith":
                 loadStudentPane("/edu/csueastbay/cs401/bsmith/reveal.fxml");
                 break;
+            case "Katherine Hettick-Leir":
+                loadStudentPane("/edu/csueastbay/cs401/khettickleir/reveal.fxml");
+                break;
             default:
                 System.out.println("huh?");
 
