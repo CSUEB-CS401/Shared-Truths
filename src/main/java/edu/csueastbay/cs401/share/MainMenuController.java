05a9b8f0726db185e8a1e912f7963c8e64f868c8
Paul Sander <psander@horizon.csueastbay.edu>
2021-10-30 10:05:39 -0700

    Add Paul SAnder's first truth

--- src/main/java/edu/csueastbay/cs401/share/MainMenuController.java
+++ src/main/java/edu/csueastbay/cs401/share/MainMenuController.java
@@ -28,6 +28,9 @@ public class MainMenuController {
             case "Bob Smith":
                 loadStudentPane("/edu/csueastbay/cs401/bsmith/reveal.fxml");
                 break;
+            case "Paul Sander":
+                loadStudentPane("/edu/csueastbay/cs401/psander/reveal.fxml");
+                break;
             default:
                 System.out.println("huh?");
 
