e33f38b68046d0d837227ec0f952313163c72dab
Tiffany <ntruong30@horizon.csueastbay.edu>
2021-11-17 18:06:52 -0800

    Working on truth, uploaded .java files, and .fxml

---
+++ src/main/java/edu/csueastbay/cs401/ttruong/revealController.java
@@ -0,0 +1,30 @@
+package edu.csueastbay.cs401.ttruong;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class revealController implements Initializable {
+
+    @FXML
+    private Label displayLabel;
+
+    @FXML
+    private Button revealButton;
+
+    @FXML
+    void reveal(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
+
