995bf950f4799fa5aa89b5f088e33c351d08c1b5
AYellowOrange <rravi2@horizon.csueastbay.edu>
2021-11-17 16:13:50 -0800

    Completed Updated Fork

--- src/main/java/edu/csueastbay/cs401/rravi/RevealController.java
+++ src/main/java/edu/csueastbay/cs401/rravi/RevealController.java
@@ -11,21 +11,31 @@ import java.util.ResourceBundle;
 
 public class RevealController implements Initializable {
     private Truth rahulsTruth;
+    private Truth secondTruth;
     @FXML
     private Label displayLabel;
+    @FXML
+    private Label displayLabel2;
 
     @FXML
     private Button revealButton;
+    @FXML
+    private Button revealButton2;
 
     @FXML
     void reveal(ActionEvent event) {
         displayLabel.setText(rahulsTruth.getAnswer());
     }
-
+    @FXML
+    void reveal2(ActionEvent event) {
+        displayLabel2.setText(secondTruth.getAnswer());
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        rahulsTruth = new Truth("Cereal or milk First?" , "Neither I don't eat cereal");
+        rahulsTruth = new Truth("Cereal or milk First?" , "Ew nah I don't eat cereal");
         displayLabel.setText(rahulsTruth.getQuestion());
+        secondTruth = new Truth("Favorite color?" , "Black");
+        displayLabel2.setText(secondTruth.getQuestion());
     }
 }
 
