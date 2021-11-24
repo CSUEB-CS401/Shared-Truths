4c5e9e64ab88dbb8ba909ae113277a7f8b04c3e9
ggamata2011 <37355821+ggamata2011@users.noreply.github.com>
2021-11-24 10:28:28 -0800

    Pull Request for Adding my Second Button for HW assignment 2 (#48)
    * Added a Second button for use
    
    * Added a Second Label for the Second Truth as well as a Second button to reveal the second truth

--- src/main/java/edu/csueastbay/cs401/ggamata/RevealController.java
+++ src/main/java/edu/csueastbay/cs401/ggamata/RevealController.java
@@ -13,12 +13,19 @@ import java.util.ResourceBundle;
 public class RevealController implements Initializable {
 
     private Truth GuillerTruth;
+    private Truth GuillerTruth2;
 
     @FXML
     private Button RevealButton;
 
     @FXML
+    private Button RevealButton2;
+
+    @FXML
     private Label displayLabel;
+
+    @FXML
+    private Label displayLabel2;
 
     @FXML
     void reveal(ActionEvent event)
@@ -26,11 +33,17 @@ public class RevealController implements Initializable {
       displayLabel.setText(GuillerTruth.getAnswer());
     }
 
+    @FXML
+    void reveal_second_truth(ActionEvent event) { displayLabel2.setText(GuillerTruth2.getAnswer());}
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
         GuillerTruth = new Truth("HomeTown", "Hayward,CA");
+        GuillerTruth2 = new Truth("Fav Food","Sushi");
         displayLabel.setText(GuillerTruth.getQuestion());
+        displayLabel2.setText(GuillerTruth2.getQuestion());
     }
 }
 
