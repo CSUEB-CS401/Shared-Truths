a784ca1ab1df0933053fc91c51c61d517f621beb
Chan Saeteurn <csaeteurn8@horizon.csueastbay.edu>
2021-11-26 17:15:04 -0800

    adding second button

---
+++ src/main/java/edu/csueastbay/cs401/csaeteurn/RevealController2.java
@@ -0,0 +1,28 @@
+package edu.csueastbay.cs401.csaeteurn;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RevealController2 implements Initializable {
+    private ChansTruth truth;
+
+    @FXML
+    private Label displayLabel;
+
+    @FXML
+    void reveal(ActionEvent event) {
+        displayLabel.setText(truth.getAnswer());
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        truth = new ChansTruth("What kind of color is my dog?", "Fawn Pied");
+        displayLabel.setText(truth.getQuestion());
+    }
+}
+
